2241d3a09ca463cc01236987ec831dd0cadb9687
Md. Al Amin <135636431+techtalent17@users.noreply.github.com>
2024-04-04 02:08:28 +0600

    C Language
    C Language. Find the small & large number

---
+++ Largest or smallest element.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int a[]={5,4,7,2,3},i,l,s;
+    l=a[0];
+    s=a[0];
+
+    for(i=0;i<5;i++)
+    {
+        if (l<a[i]);
+    }
+else if (s>a[i])
+    {
+        s=a[i];
+    }
+}
+printf("Large= %d\nSmall= %d,l,s");
+return 0;
+
